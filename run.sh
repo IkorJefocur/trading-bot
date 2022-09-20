5a902219606bdd4173ff404bc9dbb4695a33ad47
Ikor Jefocur <ikor.jfcr@gmail.com>
2022-09-20 14:12:16 +0300

    Now runninng in background on production mode

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+./stop.sh 2>/dev/null || true
 echo -e "\n---------- $(date) ----------\n" >> log.txt
-authbind pipenv run python prod 2>&1 | tee --append log.txt
+nohup pipenv run authbind python prod 2>&1 >> log.txt &
+echo $! > pid.txt
