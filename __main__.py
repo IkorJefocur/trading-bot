8876263e6e60a5872c11d74c516f04e171c25c3c
Ikor Jefocur <ikor.jfcr@gmail.com>
2022-09-22 14:43:39 +0300

    Changed development telegram chats parameter format

--- __main__.py
+++ __main__.py
@@ -10,5 +10,8 @@ run(
 		endpoint = 'https://api-testnet.bybit.com',
 		key = environ['BYBIT_KEY'], secret = environ['BYBIT_SECRET']
 	),
-	Bot(token = environ['TELEGRAM_TOKEN'], chats = [environ['TELEGRAM_CHAT']])
+	Bot(
+		token = environ['TELEGRAM_TOKEN'],
+		chats = environ['TELEGRAM_CHATS'].split(',')
+	)
 )
