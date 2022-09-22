d7ec4f3e8f3c92aa5d47a807427134b8be8a6d75
Ikor Jefocur <ikor.jfcr@gmail.com>
2022-09-22 15:18:33 +0300

    Switched from different entry points for development and production to environment config

--- __main__.py
+++ __main__.py
@@ -2,13 +2,17 @@ from os import environ
 from dotenv import load_dotenv
 from project import run, Server, Trade, Bot
 
-load_dotenv()
+load_dotenv('.env')
+load_dotenv('.prod.env')
 
 run(
-	Server(local = True),
+	Server(
+		local = True if 'LOCAL_SERVER' in environ else False
+	),
 	Trade(
 		endpoint = 'https://api-testnet.bybit.com',
-		key = environ['BYBIT_KEY'], secret = environ['BYBIT_SECRET']
+		key = environ['BYBIT_KEY'],
+		secret = environ['BYBIT_SECRET']
 	),
 	Bot(
 		token = environ['TELEGRAM_TOKEN'],
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ./stop.sh 2>/dev/null || true
 echo -e "\n---------- $(date) ----------\n" >> log.txt
-nohup pipenv run authbind python prod 2>&1 >> log.txt &
+nohup pipenv run authbind python . 2>&1 >> log.txt &
 echo $! > pid.txt
