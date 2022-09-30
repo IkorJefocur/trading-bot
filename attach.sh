500127f7404a1e9f3bdfc3cb101ef560227fcc89
Ikor Jefocur <ikor.jfcr@gmail.com>
2022-09-30 19:59:59 +0300

    Process management scripts now does not depend on cwd

--- attach.sh
+++ attach.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+cd `dirname $0`
 screen -r `cat pid.txt`
--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+cd `dirname $0`
 echo -e "\n---------- $(date) ----------\n" >> log.txt
-screen -dmS $$ bash -c "pipenv run authbind python . 2>&1 | tee log.txt"
+screen -dmS $$ bash -c "cd .. && pipenv run authbind python . | tee log.txt"
 echo $$ > pid.txt
 ./attach.sh
