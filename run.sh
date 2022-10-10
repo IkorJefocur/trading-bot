975b497bd2d80852fb10d9b49cd80620f85dd508
Ikor Jefocur <ikor.jfcr@gmail.com>
2022-10-10 23:02:39 +0300

    Added unbuffer to startup script

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 cd `dirname $0`
 ./stop.sh || true
 echo -e "\n---------- $(date) ----------\n" >> log.txt
-screen -dmS $$ bash -c "pipenv run authbind python . |& tee --append log.txt"
+screen -dmS $$ bash -c "unbuffer pipenv run authbind python . |& tee --append log.txt"
 echo $$ > pid.txt
 ./attach.sh
