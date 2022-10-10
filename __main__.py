2a796f11e4ee1437849c20331994ef2028802570
Ikor Jefocur <ikor.jfcr@gmail.com>
2022-10-10 17:39:54 +0300

    Changed main strategy

--- __main__.py
+++ __main__.py
@@ -5,14 +5,10 @@ from json import load
 from dotenv import load_dotenv
 from project.models.trader import Trader
 from project.services.http_client import HTTPClient
-from project.services.flask_server import FlaskServer
-from project.services.telegram import Telegram
 from project.services.bybit import Bybit
-from project.plugins.tradingview_server import TradingviewServer
 from project.plugins.binance_traders_watch import BinanceTradersWatch
-from project.plugins.trade import Trade
+from project.plugins.copy_trade import CopyTrade
 from project.plugins.file_manager import FileManager, TraderFormat
-from project.plugins.log import Log
 
 load_dotenv('.env')
 config = {}
@@ -20,35 +16,11 @@ if path.isfile('config.json'):
 	config = load(open('config.json', 'r'))
 
 traders_http = HTTPClient(config.get('traders_proxies', []))
-flask = FlaskServer(
-	name = __name__,
-	local = config.get('local_server', False)
-)
-telegram = Telegram(
-	api_id = environ['TELEGRAM_ID'],
-	api_hash = environ['TELEGRAM_HASH']
-).login(
-	bot_token = environ.get('TELEGRAM_TOKEN')
-)
 bybit = Bybit(
 	testnet = True,
 	key = environ['BYBIT_KEY'],
 	secret = environ['BYBIT_SECRET']
 )
-
-trade = Trade(
-	bybit
-)
-log = Log(
-	telegram,
-	chats = config.get('telegram_chats', [])
-)
-
-tv = TradingviewServer(
-	flask
-)
-tv.events.candle_created += trade.make_order
-tv.events.candle_created += log.send_candle
 
 makedirs('db/traders', exist_ok = True)
 trader_format = TraderFormat()
@@ -61,12 +33,19 @@ def make_trader_watch(uid):
 		traders_http,
 		trader = dump.load() or Trader(uid)
 	)
+	copy = CopyTrade(
+		bybit,
+		trader = watch.trader,
+		allowed_symbols = config.get('traders_symbols')
+	)
 	watch.events.trader_fetched += lambda: dump.save(watch.trader)
-	return [dump, watch]
+	watch.events.position_updated += copy.copy_position
+	return [dump, watch, copy]
 traders_watch = [*chain.from_iterable(
 	make_trader_watch(uid) for uid in config.get('traders', [])
 )]
 
-for plugin in [trade, log, tv, *traders_watch]:
+for plugin in [*traders_watch]:
 	plugin.start_lifecycle()
+print('=== STARTED ===')
 new_event_loop().run_forever()
