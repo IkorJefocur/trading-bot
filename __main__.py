b132046e093403b7ba76f5b47c788cedfe6e221f
Ikor Jefocur <ikor.jfcr@gmail.com>
2022-10-28 18:00:54 +0300

    Made copytrading alongside perpetual

--- __main__.py
+++ __main__.py
@@ -5,12 +5,12 @@ from json import load
 from dotenv import load_dotenv
 from project.models.market import Market
 from project.models.trader import Trader, User
-from project.models.strategy import CopytradingStrategy
+from project.models.strategy import TradingStrategy, CopytradingStrategy
 from project.services.http_client import HTTPClient
 from project.services.bybit import Bybit
 from project.plugins.binance_traders_watch import BinanceTradersWatch
-from project.plugins.bybit_watch import BybitCopytradingWatch
-from project.plugins.copy_trade import CopyCopytrade
+from project.plugins.bybit_watch import BybitWatch, BybitCopytradingWatch
+from project.plugins.copy_trade import CopyTrade, CopyCopytrade
 from project.plugins.file_manager import FileManager, TraderFormat
 
 load_dotenv('.env')
@@ -27,11 +27,17 @@ bybit = Bybit(
 
 makedirs('db/traders', exist_ok = True)
 trader_format = TraderFormat()
-bybit_watch = BybitCopytradingWatch(
+bybit_watch = BybitWatch(
 	bybit,
 	market = Market(),
 	user = User(0)
 )
+bybit_copytrading_watch = BybitCopytradingWatch(
+	bybit,
+	market = Market(),
+	user = User(0)
+)
+
 def make_trader_watch(uid, depo):
 	dump = FileManager(
 		path = f'db/traders/{uid}.json',
@@ -42,10 +48,22 @@ def make_trader_watch(uid, depo):
 		trader = dump.load() or Trader(),
 		uid = uid
 	)
-	copy = CopyCopytrade(
+	copy = CopyTrade(
 		bybit,
 		market = bybit_watch.market,
 		user = bybit_watch.user,
+		trader = watch.trader,
+		trading_strategy = TradingStrategy(
+			config['leverage'],
+			depo,
+			config.get('copy_amount_portion')
+		),
+		allowed_symbols = config.get('traders_symbols')
+	)
+	copytrade = CopyCopytrade(
+		bybit,
+		market = bybit_copytrading_watch.market,
+		user = bybit_copytrading_watch.user,
 		trader = watch.trader,
 		trading_strategy = CopytradingStrategy(
 			config['leverage'],
@@ -56,16 +74,19 @@ def make_trader_watch(uid, depo):
 		),
 		allowed_symbols = config.get('traders_symbols')
 	)
+
 	watch.events.trader_fetched += lambda: dump.save(watch.trader)
 	watch.events.position_updated += copy.copy_position
-	return [dump, copy, watch]
+	watch.events.position_updated += copytrade.copy_position
+	return [dump, copy, copytrade, watch]
 traders_logic = [*chain.from_iterable(
 	make_trader_watch(uid, depo) for uid, depo \
 		in config.get('traders', {}).items()
 )]
 
-for plugin in [bybit_watch, *traders_logic]:
+for plugin in [bybit_watch, bybit_copytrading_watch, *traders_logic]:
 	plugin.start_lifecycle()
 print('=== STARTED ===')
-print(f'Balance: {bybit_watch.user.deposit} USDT')
+print(f'Perpetual balance: {bybit_watch.user.deposit} USDT')
+print(f'Copytrading balance: {bybit_copytrading_watch.user.deposit} USDT')
 new_event_loop().run_forever()
