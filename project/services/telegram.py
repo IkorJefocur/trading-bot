6a4f315c2d89b40888e4c8ac0259571f14379c57
Ikor Jefocur <ikor.jfcr@gmail.com>
2022-11-18 20:32:16 +0300

    Refactored telegram service

--- project/services/telegram.py
+++ project/services/telegram.py
@@ -3,13 +3,24 @@ from ..base import Service
 
 class Telegram(Service):
 
-	def __init__(self, api_id, api_hash):
-		super().__init__(TelegramClient(None, api_id, api_hash))
+	def __init__(self, api_id, api_hash, token = None):
+		super().__init__(None)
+		self.target = TelegramClient(None, api_id, api_hash, loop = self.loop)
+		self.token = token
 
-	def login(self, **params):
-		self.loop.run_until_complete(self.target.start(**params))
-		return self
+	def start_lifecycle(self):
+		if not self.target._authorized:
+			self.loop.run_until_complete(self.login())
+		super().start_lifecycle()
 
 	def run(self):
-		while True:
-			self.target.run_until_disconnected()
+		self.target.run_until_disconnected()
+
+	def stop(self):
+		self.run_task_sync(self.disconnect())
+
+	async def login(self):
+		await self.target.start(bot_token = self.token)
+
+	async def disconnect(self):
+		await self.target.disconnect()
