1cdab2c32cac294b3d050efd4605fc14a009e174
Ikor Jefocur <ikor.jfcr@gmail.com>
2022-12-26 16:08:49 +0300

    Added switch to hedge mode before opening order

--- project/plugins/copy_trade.py
+++ project/plugins/copy_trade.py
@@ -1,9 +1,23 @@
+from asyncio import gather
 from datetime import timedelta, datetime
 from pybit import InvalidRequestError
 from ..base import Plugin
 from ..models.strategy import TradingStrategy, CopytradingStrategy
 
 class CopyTrade(Plugin):
+
+	@staticmethod
+	def error_safe_request(codes):
+		def decorate(method):
+			@Plugin.loop_bound
+			async def run(*args, **kwargs):
+				try:
+					return await method(*args, **kwargs)
+				except InvalidRequestError as error:
+					if error.status_code not in codes:
+						raise
+			return run
+		return decorate
 
 	def __init__(
 		self, bybit_service, market, user, trader, trading_strategy,
@@ -19,7 +33,10 @@ class CopyTrade(Plugin):
 
 	async def copy_position(self, base):
 		if self.market.coin(base.symbol) and self.filter_position(base):
-			await self.set_leverage(base.symbol, base.leverage)
+			await gather(
+				self.set_leverage(base.symbol, base.leverage),
+				self.set_mode(base.symbol)
+			)
 			await self.copy_position_strategy(base)
 
 	@Plugin.loop_bound
@@ -42,18 +59,20 @@ class CopyTrade(Plugin):
 			)
 			self.user.update_position(position)
 
-	@Plugin.loop_bound
+	@error_safe_request((34036,))
 	async def set_leverage(self, symbol, leverage):
-		not_modified_code = 34036
-		try:
-			self.service.usdt_perpetual.set_leverage(
-				symbol = symbol.value,
-				buy_leverage = leverage,
-				sell_leverage = leverage
-			)
-		except InvalidRequestError as error:
-			if error.status_code != not_modified_code:
-				raise
+		self.service.usdt_perpetual.set_leverage(
+			symbol = symbol.value,
+			buy_leverage = leverage,
+			sell_leverage = leverage
+		)
+
+	@error_safe_request((30083,))
+	async def set_mode(self, symbol):
+		self.service.usdt_perpetual.position_mode_switch(
+			symbol = symbol.value,
+			mode = 'BothSide'
+		)
 
 	def filter_position(self, position):
 		return not self.allowed_symbols \
@@ -93,16 +112,14 @@ class CopyCopytrade(CopyTrade):
 				self.user.place_order(order)
 				self.user.update_position(position)
 
-	@Plugin.loop_bound
+	@CopyTrade.error_safe_request((34036,))
 	async def set_leverage(self, symbol, leverage):
-		not_modified_code = 34036
-		try:
-			self.service.http.post(
-				'/contract/v3/private/copytrading/position/set-leverage',
-				symbol = symbol.value,
-				buy_leverage = str(leverage),
-				sell_leverage = str(leverage)
-			)
-		except InvalidRequestError as error:
-			if error.status_code != not_modified_code:
-				raise
+		self.service.http.post(
+			'/contract/v3/private/copytrading/position/set-leverage',
+			symbol = symbol.value,
+			buy_leverage = str(leverage),
+			sell_leverage = str(leverage)
+		)
+
+	async def set_mode(self, symbol):
+		pass
